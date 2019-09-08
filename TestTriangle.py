6fdbc8c20350888bc5098364472d4ab5a5265b8d
Josh Pirog <Josh@MacBook-Pro.home>
2019-09-08 17:38:51 -0400

    nobug code

--- TestTriangle.py
+++ TestTriangle.py
@@ -16,17 +16,35 @@ from Triangle import classifyTriangle
 
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
-
+    
+    def testValidTriangle(self):
+        self.assertEqual(classifyTriangle(4,2,10),'NotATriangle')
+    
+    def testValidInput(self):
+        self.assertEqual(classifyTriangle(201, 500, -5), 'InvalidInput')
+        
+    def testIsIntegers(self):
+        self.assertEqual(classifyTriangle("apple", "pear", "fig"), 'InvalidInput')
+        
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5,3,4),'Right')
         
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-
+    def testEquilateralTriangle(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
+    
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(8, 7, 5), 'Scalene')
+    
+    def testIsocelesTriangle(self):
+        self.assertEqual(classifyTriangle(5, 7, 5), 'Isosceles')
+        
+    
+        
+    
 if __name__ == '__main__':
     print('Running unit tests')
-    unittest.main()
+    unittest.main(exit=False, verbosity=2)
 
--- Triangle.py
+++ Triangle.py
@@ -5,7 +5,7 @@ Updated Jan 21, 2018
 
 The primary goal of this file is to demonstrate a simple python program to classify triangles
 
-@author: jrr
+@author: Joshua Pirog
 @author: rk
 """
 
@@ -22,7 +22,7 @@ def classifyTriangle(a,b,c):
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
+        If the sum of any two sides equals the square of the third side, then return 'Right'
       
       BEWARE: there may be a bug or two in this code
     """
@@ -31,7 +31,7 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
@@ -42,16 +42,16 @@ def classifyTriangle(a,b,c):
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    # or the specified shape is not a triangle
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
     if a == b and b == a:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((b ** 2) + (c ** 2)) == (a ** 2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and  (a != c) and (b != c):
         return 'Scalene'
     else:
-        return 'Isoceles'
+        return 'Isosceles'
